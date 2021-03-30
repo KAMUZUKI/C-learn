c1f198172ad78f629fe9161fc0c8423c60435d1c
KAMUZUKI <1437487442@qq.com>
2021-03-30 14:19:46 +0800

    C++sizeof计算关键字

---
+++ CPP_ex/3/21_3_25/test_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<complex>
+using namespace std;
+int main()
+{
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(short int)<<endl;
+    cout<<sizeof(long int)<<endl;
+    cout<<sizeof(long long int)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(bool)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(complex<float>)<<endl;
+    cout<<sizeof(complex<double>)<<endl;
+    cout<<sizeof(complex<long long>)<<endl;
+    system("pause");
+
+    return 0;
+}
