89040d6b568a5adcb513f36a18ad1f8fcdbe6094
KAMUZUKI <1437487442@qq.com>
2021-04-21 23:57:51 +0800

    this指针概念

---
+++ CPP_ex/4/21_4_18/test_3.cpp
@@ -0,0 +1,34 @@
+//this指针概念
+#include<iostream>
+#include<string>
+using namespace std;
+class Person
+{
+public:
+    Person(int age)
+    {
+        //1、当形参和成员变量同名时，可用this指针来区分
+        this->age = age;
+    }
+    Person& PersonAddPerson(Person p)
+    {
+        this->age += p.age;
+        //返回对象本身
+        return *this;
+    }
+int age;
+};
+void test()
+{
+    Person p1(10);
+    cout<<"p1.age = "<<p1.age<<endl;
+    Person p2(10);
+    p2.PersonAddPerson(p1).PersonAddPerson(p1).PersonAddPerson(p1);
+    cout << "p2.age = " << p2.age << endl;
+}
+int main()
+{
+    test();
+    system("pause");
+    return 0;
+}
