1df48ae418f2319797b59da6ae88a04f7e1b7df4
KAMUZUKI <1437487442@qq.com>
2021-05-04 12:56:05 +0800

    菱形继承

---
+++ CPP_ex/5/21_5_3/test_5.cpp
@@ -0,0 +1,40 @@
+//4.6.8 菱形继承
+// 菱形继承概念：
+// 两个派生类继承同一个基类
+// ?又有某个类同时继承者两个派生类
+// ?这种继承被称为菱形继承，或者钻石继承
+#include<iostream>
+#include<string>
+using namespace std;
+class Animal
+{
+public:
+	int m_Age;
+};
+
+//继承前加virtual关键字后，变为虚继承
+//此时公共的父类Animal称为虚基类
+class Sheep : virtual public Animal {};
+class Tuo   : virtual public Animal {};
+class SheepTuo : public Sheep, public Tuo {};
+
+void test01()
+{
+	SheepTuo st;
+	st.Sheep::m_Age = 100;
+	st.Tuo::m_Age = 200;
+
+	cout << "st.Sheep::m_Age = " << st.Sheep::m_Age << endl;
+	cout << "st.Tuo::m_Age = " <<  st.Tuo::m_Age << endl;
+	cout << "st.m_Age = " << st.m_Age << endl;
+}
+
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
